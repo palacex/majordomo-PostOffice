0caa2166b2310f3dd1df89510094f19655f28be0
LDV <palacex@gmail.com>
2015-01-29 17:55:33 +0300

    multi-language support

---
+++ languages/app_postoffice_default.php
@@ -0,0 +1,50 @@
+﻿<?php
+/**
+ * Default language file for PostOffice application
+ *
+ * @package PostOffice
+ * @author Lutsenko D.V. <palacex@gmail.com> http://silvergate.ru/
+ * @version 1.0
+ */
+
+$dictionary = array(
+'POSTOFFICE_APP_TITLE'=>'Russian Post - Post tracking',
+'POSTOFFICE_TAB_CHECK'=>'Check parcels',
+'POSTOFFICE_TAB_PROXY' => 'Proxy',
+'POSTOFFICE_TAB_NOTIFICATION' => 'Notification',
+'POSTOFFICE_TRACK_INDEX' => '#',
+'POSTOFFICE_TRACK_NUMBER' => 'Track number',
+'POSTOFFICE_TRACK_NAME' => 'Track name',
+'POSTOFFICE_TRACK_URL' => 'Link',
+'POSTOFFICE_TRACK_ADD' => 'Add',
+'POSTOFFICE_TRACK_CHECK' => 'Check',
+'POSTOFFICE_TRACK_DATE' => 'Date',
+'POSTOFFICE_TRACK_LM_DATE' => 'Operation Date',
+'POSTOFFICE_TRACK_CONDITION' => 'Condition',
+'POSTOFFICE_TRACK_LOCATION' => 'Current location',
+'POSTOFFICE_TRACK_STATUS' => 'Status',
+'POSTOFFICE_PROXY_USE' => 'Use proxy',
+'POSTOFFICE_PROXY_ADDRESS' => 'Proxy address',
+'POSTOFFICE_PROXY_PORT' => 'Port',
+'POSTOFFICE_PROXY_LOGIN' => 'Login',
+'POSTOFFICE_PROXY_PASSWORD' => 'Password',
+'POSTOFFICE_ACTION_CHANGE' => 'Change',
+'POSTOFFICE_ACTION_CLOSE' => 'Close',
+'POSTOFFICE_ACTION_CANCEL' => 'Cancel',
+'POSTOFFICE_ACTION_DELETE' => 'Delete',
+'POSTOFFICE_NOTIFY_BY_EMAIL' => 'Notify sending parcels via email',
+'POSTOFFICE_NOTIFY_EMAIL_ADDRESS' => 'Notify address',
+'POSTOFFICE_NOTIFY_EMAIL_SUBJECT' => 'Subject',
+'POSTOFFICE_CONFIRM_DELETE_TITLE' => 'Removal Confirmation track numbers',
+'POSTOFFICE_CONFIRM_DELETE_BODY' =>  'Are you sure you want to delete this track and all the information about it?'
+);
+
+
+foreach ($dictionary as $k=>$v)
+{
+   if (!defined('LANG_' . $k))
+   {
+      define('LANG_'. $k, $v);
+   }
+}
+?>
---
+++ languages/app_postoffice_ru.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Default language file for PostOffice application
+ *
+ * @package PostOffice
+ * @author Lutsenko D.V. <palacex@gmail.com> http://silvergate.ru/
+ * @version 1.0
+ */
+
+$dictionary = array(
+'POSTOFFICE_APP_TITLE'=>'Почта Росссии - Отслеживание посылок',
+'POSTOFFICE_TAB_CHECK'=>'Проверка почты',
+'POSTOFFICE_TAB_PROXY' => 'Настройка прокси',
+'POSTOFFICE_TAB_NOTIFICATION' => 'Настройка уведомлений',
+'POSTOFFICE_TRACK_INDEX' => '№',
+'POSTOFFICE_TRACK_NUMBER' => 'Номер трека',
+'POSTOFFICE_TRACK_NAME' => 'Название трека',
+'POSTOFFICE_TRACK_URL' => 'Ссылка',
+'POSTOFFICE_TRACK_ADD' => 'Добавить',
+'POSTOFFICE_TRACK_CHECK' => 'Проверить',
+'POSTOFFICE_TRACK_DATE' => 'Дата',
+'POSTOFFICE_TRACK_LM_DATE' => 'Дата обновления',
+'POSTOFFICE_TRACK_CONDITION' => 'Состояние',
+'POSTOFFICE_TRACK_LOCATION' => 'Текущее местонахождение',
+'POSTOFFICE_TRACK_STATUS' => 'Статус',
+'POSTOFFICE_PROXY_USE' => 'Использовать прокси',
+'POSTOFFICE_PROXY_ADDRESS' => 'Адрес прокси',
+'POSTOFFICE_PROXY_PORT' => 'Порт',
+'POSTOFFICE_PROXY_LOGIN' => 'Логин',
+'POSTOFFICE_PROXY_PASSWORD' => 'Пароль',
+'POSTOFFICE_ACTION_CHANGE' => 'Изменить',
+'POSTOFFICE_ACTION_CLOSE' => 'Закрыть',
+'POSTOFFICE_ACTION_CANCEL' => 'Отменить',
+'POSTOFFICE_ACTION_DELETE' => 'Удалить',
+'POSTOFFICE_NOTIFY_BY_EMAIL' => 'Оповещать о доставке посылки по email',
+'POSTOFFICE_NOTIFY_EMAIL_ADDRESS' => 'Адрес для отправки уведомления',
+'POSTOFFICE_NOTIFY_EMAIL_SUBJECT' => 'Тема сообщения',
+'POSTOFFICE_CONFIRM_DELETE_TITLE' => 'Подтверждение удаления трек номера',
+'POSTOFFICE_CONFIRM_DELETE_BODY' =>  'Вы уверены в том, что хотите удалить данный трек и всю информацию о нём?'
+);
+
+foreach ($dictionary as $k=>$v)
+{
+   if (!defined('LANG_' . $k))
+   {
+      define('LANG_'. $k, $v);
+   }
+}
+?>
